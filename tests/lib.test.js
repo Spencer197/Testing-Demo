ddeca09be2a5053997d67b4fd155a99c6519619f
Spencer197 <spncr@protonmail.com>
2023-11-23 20:15:54 +0900

    Add 3 tests to lib.test.js

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,16 @@
-test('Our first test', () => {
-    throw new Error('Something failed!');
+const lib = require('../lib');
+
+test('absolute - Should return a positive number if input is positve.', () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
 });
+
+test('absolute - Should return a positive number if input is negative.', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  test('absolute - Should return 0 if input is 0.', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(1);
+  });
