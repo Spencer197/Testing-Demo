d7338456619690a315aa6b85ef5275ba4e793367
Spencer197 <spncr@protonmail.com>
2023-11-23 20:38:36 +0900

    Group related tests in a describe block

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,19 @@
 const lib = require('../lib');
 
-test('absolute - Should return a positive number if input is positve.', () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
+describe('absolute', () => {
+    it('Should return a positive number if input is positve.', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+      });
+      
+      it('Should return a positive number if input is negative.', () => {
+          const result = lib.absolute(-1);
+          expect(result).toBe(1);
+        });
+      
+        it('Should return 0 if input is 0.', () => {
+          const result = lib.absolute(0);
+          expect(result).toBe(1);
+        });
 });
 
-test('absolute - Should return a positive number if input is negative.', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-  });
-
-  test('absolute - Should return 0 if input is 0.', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(1);
-  });
